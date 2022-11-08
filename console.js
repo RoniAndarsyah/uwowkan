b7755421671a2ed2ac73ed8d5fa57ce78f239ffe
raynozapparel <117721683+raynozapparel@users.noreply.github.com>
2022-11-08 16:11:26 +0700

    wew

--- console.js
+++ console.js
@@ -2,3 +2,9 @@
 console.log("hello world")
 console.log("hello world") // ke 2
 
+
+console.log("hello world") // ke 2
+console.log("hello world") // ke 2
+console.log("hello world") // ke 2
+console.log("hello world") // ke 2
+
